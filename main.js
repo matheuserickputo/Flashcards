1c74638e2ca5df40613f965688fa77cf956f7657
Matheusck10 <107003553+matheuserickputo@users.noreply.github.com>
2024-09-19 08:47:49 -0300

    .

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-function criaCartão(){
+function criaCartão(categoria,pergunta,resposta){
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML = `<div class="conteudo-cartao">
+    <h3>${categoria}</h3>
+    <div class="pergunta-cartao">
+    <h3>${pergunta}</h3>
+    <div class="resposta-cartao">
+    <h3>${resposta}</h3>
+    </div>
+    </div>`
 }
