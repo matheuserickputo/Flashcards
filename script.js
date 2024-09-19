9956404dcae261ef413c1cae683ae12fd7e7a695
Matheusck10 <107003553+matheuserickputo@users.noreply.github.com>
2024-09-19 07:43:45 -0300

    a

---
+++ script.js
@@ -0,0 +1,39 @@
+criaCartão('fisica','o que a mecânica quântica estuda?','comportamento e interação da matéria e da energia em escala subatômica');
+
+criaCartão('física','qual a lei da relatividade?','As leis da Física são as mesmas para todos os observadores situados em referenciais inerciais. Não existe um referencial absoluto');
+
+criaCartão('física','para que serve física nuclear?','As aplicações da Física Nuclear vão desde ciência dos materiais até energia nuclear)');
+
+criaCartão('física','o que é a eletrônica?','área da física que estuda o movimento das cargas elétricas');
+
+criaCartão('química','o que são ligações químicas?','união dos átomos para a formação das moléculas');
+
+criaCartão('química','o que é estrutura atômica?','é a composição de um átomo)');
+
+criaCartão('química','quais os tipos de compostos inorgânicos?','ácido, base, sais e óxidos');
+
+criaCartão('química','quais os tipos de reações químicas?','síntese, decomposição, simples troca ou dupla troca');
+
+criaCartão('matemática','qual o Teoremas de Tales?','duas retas transversais intersectam um feixe de retas paralelas');
+
+criaCartão('matemática','Onde utiliza matrizes?','A matriz é comumente utilizada para a organização de dados tabulares a fim de facilitar a resolução de problemas');
+
+criaCartão('matemática','como se calcula uma P.G.?','com a fórmula a1.q(n-1)');
+
+criaCartão ('matemática','o que é um conjunto numérico?','é um grupo de números que possuem características em comum)');
+
+criaCartão('biologia','o que é genética molecular?','é a área da biologia que estuda a estrutura e a função dos genes a nível molecular');
+
+criaCartão('biologia','Qual é a ideia principal da evolução biológica?','todos os seres vivos compartilham um mesmo ancestral');
+
+criaCartão('biologia','exemplos de relações ecológicas:','O mutualismo, o comensalismo, o parasitismo e a predação');
+
+criaCartão('biologia','o que é biotecnologia?','é o conjunto de procedimentos envolvendo manipulação de organismos vivos para fabricar ou modificar produtos');
+
+criaCartão('português','O que é sintaxe?','trata-se do conjunto das regras que determinam as diferentes possibilidades de associação das palavras da língua para a formação de enunciados');
+
+criaCartão('português','o que é variação linguística?é o fenômeno natural que ocorre quando a língua se diversifica devido a mudanças em seus elementos');
+
+criaCartão ('português','O que é Morfologia?','trata-se do estudo a respeito da estrutura, formação e classificação das palavras isoladas de seu contexto e de suas funções nas orações e nos períodos');
+
+criaCartão ('português','o que é semântica?','é a área da linguística que estuda o significado e a sua relação com o significante')
